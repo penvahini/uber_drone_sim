a3759256f1790461675a141414ccbea9c88696c3
vahini <penma013@umn.edu>
2025-04-09 15:53:51 -0500

    drone simulation files cloned

---
+++ build/libs/libtransit.a/src/DroneObserver.d
@@ -0,0 +1,8 @@
+../../build/libs/libtransit.a/src/DroneObserver.o: src/DroneObserver.cc \
+ include/IEntityObserver.h include/Publishcer.h include/IListener.h
+
+include/IEntityObserver.h:
+
+include/Publishcer.h:
+
+include/IListener.h:
---
+++ build/libs/libtransit.a/src/Publishcer.d
@@ -0,0 +1,6 @@
+../../build/libs/libtransit.a/src/Publishcer.o: src/Publishcer.cc \
+ include/Publishcer.h include/IListener.h
+
+include/Publishcer.h:
+
+include/IListener.h:
